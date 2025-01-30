a0a291defef0046cfc96d2ee5bf10cf7fd1bd4aa
Sergiy Myronenko <Sergiymyronenko.web@gmail.com>
2025-01-30 14:17:25 +0200

    add derective and pipe update component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LeftSideMenuComponent } from './components/left-side-menu/left-side-menu.component';
+import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { PreparationComponent } from './components/preparation/preparation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    LeftSideMenuComponent,
+    TopMenuComponent,
+    PreparationComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-course-app';
--- src/app/components/preparation/preparation.component.ts
+++ src/app/components/preparation/preparation.component.ts
@@ -1,11 +1,119 @@
-import { Component } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteConfirmationModalComponent } from '../delete-confirmation-modal/delete-confirmation-modal.component';
+import { QuestionItem } from '../category/category.component.config';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, switchMap, takeUntil } from 'rxjs';
+import { PreparationService } from '../../services/preparation.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { GenerateAnswerModalComponent } from '../generate-answer-modal/generate-answer-modal.component';
 
 @Component({
   selector: 'app-preparation',
-  imports: [MatTableModule, MatButtonModule],
+  standalone: true,
+  imports: [MatTableModule, MatButtonModule, MatProgressSpinnerModule],
   templateUrl: './preparation.component.html',
-  styleUrl: './preparation.component.css',
+  styleUrl: './preparation.component.scss',
 })
-export class PreparationComponent {}
+export class PreparationComponent implements OnInit, OnDestroy {
+  displayedColumns: string[] = ['position', 'question', 'actions'];
+  dataSource = new MatTableDataSource<QuestionItem>();
+  category: string = '';
+  isLoading = false;
+
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    public dialog: MatDialog,
+    public preparationService: PreparationService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams
+      .pipe(
+        takeUntil(this.destroy$),
+        switchMap((queryParams) => {
+          this.category = queryParams['tabName'] || '';
+          this.isLoading = true;
+          return this.preparationService.getPreparationQuestionsByCategory(
+            this.category
+          );
+        })
+      )
+      .subscribe((response) => {
+        this.isLoading = false;
+        this.dataSource = response.data as any;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  updateAnswer(question: Partial<QuestionItem>, id: number): void {
+    this.preparationService
+      .updatePreparationQuestionById(question, id)
+      .pipe(
+        switchMap(() =>
+          this.preparationService.getPreparationQuestionsByCategory(
+            this.category
+          )
+        )
+      )
+      .subscribe((response) => {
+        console.log(response);
+        this.dataSource = response.data as any;
+      });
+  }
+
+  openGenerateDialog(question: QuestionItem, index: number): void {
+    const dialogRef = this.dialog.open(GenerateAnswerModalComponent, {
+      width: '500px',
+      data: {
+        ...question,
+        index,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result: string) => {
+      console.log('The dialog was closed', result);
+      if (result) {
+        this.updateAnswer({ answer: result }, question.id);
+      }
+    });
+  }
+
+  openDeleteDialog(question: QuestionItem): void {
+    const dialogRef = this.dialog.open(DeleteConfirmationModalComponent, {
+      width: '333px',
+    });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      console.log('The dialog was closed', result);
+      if (result) {
+        console.log('Question would be deleted.', question);
+        this.deleteAnswer(this.category, question.id);
+      }
+    });
+  }
+
+  deleteAnswer(categoryName: string, id: number): void {
+    this.preparationService
+      .deletePreparationQuestionById(categoryName, id)
+      .pipe(
+        switchMap(() =>
+          this.preparationService.getPreparationQuestionsByCategory(
+            this.category
+          )
+        )
+      )
+      .subscribe((response) => {
+        console.log(response);
+        this.dataSource = response.data as any;
+      });
+  }
+}
